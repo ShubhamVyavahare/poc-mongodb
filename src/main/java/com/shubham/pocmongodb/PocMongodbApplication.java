13bdba3b7c0d2a71fe0766dd22fdfe6e476cc7f0
ShubhamVyavahare <shubhamvyavahare203@gmail.com>
2024-02-24 12:50:45 +0530

    Initial Commit

---
+++ src/main/java/com/shubham/pocmongodb/PocMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.pocmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMongodbApplication.class, args);
+	}
+
+}
